69175ed5d9329b56bd5eccb46e975e40844dee41
Usernameorsmth <simidarawilliams@gmial.com>
2025-05-03 19:00:31 +0100

    GNARLY

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-#Input a word or sentence
-string = input("Please enter your own String : ")
+# input number greater than 1
+n = int(input("Enter the value of n: "))
 
-string2 = ('')
-#loop for printing in reverse
-for i in string:
-    string2 = i + string2
+# print the numbers from n to 1
+print ("numbers from {} to {} are: ".format(n,1))
 
-print("\nThe Original String =", string)
-print("The Reversed String =", string2)
+# loop to print numbers
+for i in range(n,0,-1):
+    print (i)
