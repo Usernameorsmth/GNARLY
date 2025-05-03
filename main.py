5a20350bbf594a8c31f39d844c0433289166f430
Usernameorsmth <simidarawilliams@gmial.com>
2025-05-03 18:55:04 +0100

    THING'S

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#input an integer value
-n = int(input("Enter the number whose sum you want to find: "))
-sum=0
+#Input a word or sentence
+string = input("Please enter your own String : ")
 
-#Iterates for n+1 times: i=1 to n+1
-for i in range(1,n+1):
-    sum=sum+i
-    print("\nSum =",sum)
+string2 = ('')
+#loop for printing in reverse
+for i in string:
+    string2 = i + string2
+
+print("\nThe Original String =", string)
+print("The Reversed String =", string2)
